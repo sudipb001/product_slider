ddceca70b937c4a2cf7f31d38ac30637415a3857
Sudip Bhattacharyya <sudipbhattacharyya@Sudips-MacBook-Air.local>
2022-01-27 12:17:49 +0530

    changes for jquery

--- js/lightslider.js
+++ js/lightslider.js
@@ -1,7 +1,3 @@
-var script = document.createElement('script')
-script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js'
-script.type = 'text/javascript'
-document.getElementsByTagName('head')[0].appendChild(script)
 ;(function ($, undefined) {
   'use strict'
   var defaults = {
